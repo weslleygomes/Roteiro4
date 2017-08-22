99db27acbe02bd71813cbc2e4db7243da36692f3
weslleygomes <30627516+weslleygomes@users.noreply.github.com>
2017-08-22 18:28:35 -0300

    Update Roteiro4.4.cpp

--- Roteiro4.4.cpp
+++ Roteiro4.4.cpp
@@ -1,82 +1,56 @@
 #include <stdio.h>
 #include <math.h>
+#include <iostream>
+using namespace std;
 
 struct tJogadores{
 	float votos;
 };
-
-
-
 int main(){
 
 	tJogadores jogadores[23];
-
 	int votacao = 1, i, c=0;
-
 	int maisvotado = 0;
-
 	float maisvotos =0;
 
-	
-
 	for(i=0;i<23;i++){
-
 		jogadores[i].votos = 0;
-
 	}
 
-	
+	cout << "Enquete: Quem foi o melhor jogador?" << endl;
 
 	while(votacao != 0){
-
-		printf("qual jogador vc quer votar?: ");
-
+		printf("Numero do jogador (0=fim): ");
 		scanf("%d", &votacao);
 
 		if(votacao == 0){
-
 			break;
-
 		}
-
 		if(votacao>23 ||votacao < 1){
-
-			printf("jogador invalido!\n");
-
+			printf("Informe um valor entre 1 e 23 ou 0 para sair!\n");
 			continue;
-
 		}
-
 		jogadores[votacao-1].votos += 1;
-
 		c++;
-
 	}
+
+	cout << "------------------------------------------" << endl;
 
 	for(i=0;i<23;i++){
-
 		if (jogadores[i].votos == 0){
-
 			continue;
-
 		}
-
-		printf("votos do jogador %d: %.02f\n",i+1, (jogadores[i].votos/c)*100);
-
+		printf("Votos do jogador %d: %.01f%%\n",i+1, (jogadores[i].votos/c)*100);
 	}
+
+	cout << "------------------------------------------" << endl;
 
 	for(i = 0; i<23;i++){
-
 		if(jogadores[i].votos > maisvotos){
-
 			maisvotos = jogadores[i].votos;
-
 			maisvotado = i;
-
 		}
-
 	}
-
-	printf("O jogador %d foi o mais votado com %f porcento dos votos!",maisvotado+1, ((jogadores[maisvotado].votos/c)*100) );
-
+	
+	printf("O jogador %d foi o mais votado com %.01f%% dos votos!\n",maisvotado+1, ((jogadores[maisvotado].votos/c)*100) );
 }
